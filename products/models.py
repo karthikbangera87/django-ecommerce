fd35adb7b9a35aa98fe9ae5e840b5dae06709a36
Karthik <karthikbangera87@gmail.com>
2015-05-27 23:52:10 -0400

    building up the products app and static file rendering

--- products/models.py
+++ products/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=50, decimal_places=2)
+    slug = models.SlugField()
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+    active = models.BooleanField(default=True)
+
+    def __unicode__(self):
+        return self.title
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product)
+    image = models.ImageField(upload_to='templates/img/')
+    featured = models.BooleanField(default=False)
+    thumbnail = models.BooleanField(default=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __unicode__(self):
+        return self.product.title
--- products/views.py
+++ products/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
+
 def home(request):
-	context = {"test": "Karthik"}
-	template = 'base.html'
-	return render(request,template,context)
+    context = {"test": "Karthik"}
+    template = 'base.html'
+    return render(request, template, context)
+
+
+def all(request):
+
+    all_products = Product.objects.all()
+    context = {'all_products': all_products}
+    template = 'product.html'
+    return render(request, template, context)
